9cf85374548676a6ab0ff3a292655a53512978c2
justinsorensen2 <justinsorensen2@gmail.com>
2020-02-10 15:05:15 -0500

    'created project!'

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //write hello and my name to console
+      var myName = "Justin";
+      Console.WriteLine("Hello, World, my name is " + myName + ".");
+    }
+  }
+}
